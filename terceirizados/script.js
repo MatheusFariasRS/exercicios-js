1d88cbe5fafa33ce757236a80f8849c451c76a65
Matheus Farias <matheusaf.contato@gmail.com>
2025-03-20 16:12:06 -0300

    terceirizados

---
+++ terceirizados/script.js
@@ -0,0 +1,78 @@
+const dados = [
+  {
+    nome: "Maria Silva",
+    valorHora: 80.0,
+    horasTrabalhadas: 20,
+    terceirizado: true,
+  },
+  {
+    nome: "João Rocha",
+    valorHora: 60.0,
+    horasTrabalhadas: 25,
+    terceirizado: false,
+  },
+  {
+    nome: "Tiago James",
+    valorHora: 80.0,
+    horasTrabalhadas: 10,
+    terceirizado: true,
+  },
+  {
+    nome: "Ana Carla",
+    valorHora: 70.0,
+    horasTrabalhadas: 40,
+    terceirizado: true,
+  },
+  {
+    nome: "Pedro Paulo",
+    valorHora: 50.0,
+    horasTrabalhadas: 25,
+    terceirizado: false,
+  },
+];
+
+//---------------------------------------------------------------------------
+// FUNCAO funcionariosTerceirizados
+// Parâmetros:
+//   list: lista de funcionarios
+// Efeito: retorna uma lista contendo somente os funcionários terceirizados
+//---------------------------------------------------------------------------
+
+
+
+function funcionariosTerceirizados(list) {
+  let funcionario = [];
+  for(let i =0; i < list.length; i++){
+        if(list[i].terceirizado == true){
+           funcionario.push(list[i]);
+        }
+  }
+        return funcionario;
+}
+
+//---------------------------------------------------------------------------
+// FUNCAO custoTotal
+// Parâmetros:
+//   list: lista de funcionarios
+// Efeito: retorna um número representando o custo total dos funcionários
+//---------------------------------------------------------------------------
+
+function custoTotal(list) {
+  let sum = 0;
+  for(let i =0; i < list.length; i++){
+    sum += list[i].valorHora * list[i].horasTrabalhadas;
+  }
+  return sum;
+}
+
+//---------------------------------------------------------------------------
+// SCRIPT PRINCIPAL
+//---------------------------------------------------------------------------
+
+const terceirizados = funcionariosTerceirizados(dados);
+
+const custoTerceirizados = custoTotal(terceirizados);
+
+console.log(
+  "Custo total dos funcionarios terceirizados: R$ " + custoTerceirizados
+);
